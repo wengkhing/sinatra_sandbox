1a4a23d87468730899c7a17ff13d8e38acbda74f
wengkhing <wengkhing@gmail.com>
2014-11-27 10:28:22 +0800

    get and post

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,17 @@
 get '/' do
-  @aunty = params[:aunty]
-  puts @aunty
-  # Look in app/views/index.erb
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
 end
 
-post '/aunty' do
-  speech = params[:user_input]
-  redirect to("/?aunty=#{speech}")
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_cool_url
 end
